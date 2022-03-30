9c21d1ab51eed406a6ac001372bd494ce81721a5
idontknowstuff <zurizheng88@gmail.com>
2022-03-29 20:51:53 -0600

    file organization

--- phaser/myweb/src/index.js
+++ phaser/myweb/src/index.js
@@ -1,9 +1,10 @@
-import load from 'load.js';
-import mainscreen from 'mainscreen.js';
-import modes from 'modes.js';
-import singlePlayer from 'singlePlayer.js';
-import Duel from 'duel.js';
-import DuelPrep from 'duelprep.js';
+import Phaser from 'phaser';
+import load from './scenes/load.js';
+import mainscreen from './scenes/mainscreen.js';
+import modes from './scenes/modes.js';
+import singlePlayer from './scenes/singlePlayer.js';
+import Duel from './scenes/duel.js';
+import DuelPrep from './scenes/duelprep.js';
 
 var config = {
     width: 1000,
