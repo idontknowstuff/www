05a5343d6e7a7c27268d87d842d26b0faff0b655
idontknowstuff <zurizheng88@gmail.com>
2022-03-29 18:15:46 -0600

    set up server, has error

--- phaser/myweb/src/game.js
+++
@@ -1,8 +0,0 @@
-var config = {
-    width: 1000,
-    height: 600,
-    backgroundColor: 0x000000,
-    scene: [load, mainscreen, modes, singlePlayer, Duel, DuelPrep]
-}
-
-var game = new Phaser.Game(config);
---
+++ phaser/myweb/src/index.js
@@ -0,0 +1,8 @@
+var config = {
+    width: 1000,
+    height: 600,
+    backgroundColor: 0x000000,
+    scene: [load, mainscreen, modes, singlePlayer, Duel, DuelPrep]
+}
+
+var game = new Phaser.Game(config);
