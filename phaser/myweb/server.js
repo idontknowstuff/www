0bbb734832f5ad4b6b0b80092734e3721614f7e1
idontknowstuff <zurizheng88@gmail.com>
2022-05-22 20:48:13 -0600

    more "me" understand friendly

--- phaser/myweb/server.js
+++ phaser/myweb/server.js
@@ -13,5 +13,5 @@ app.get('*', (req, res) => {
 const PORT = process.env.PORT || 8080;
 
 app.listen(PORT, () => {
-  console.log(`Server running at http:/`);
+  console.log(`Server running at http:/` + PORT);
 });
