6e3468f01b89e2a390730754d5f77ca4eac96380
idontknowstuff <zurizheng88@gmail.com>
2022-06-02 21:40:32 -0600

    server changes

--- phaser/myweb/server.js
+++ phaser/myweb/server.js
@@ -11,26 +11,24 @@ app.get('*', (req, res) => {
 });
 app.get('/times', (req, res) => res.send(showTimes()))
 
-const { Pool } = require('pg');
-const pool = new Pool({
+const { Client } = require('pg');
+const client = new Client({
   connectionString: process.env.DATABASE_URL,
   ssl: {
     rejectUnauthorized: false
   }
 });
 
-app.get('/db', async (req, res) => {
-  try {
-    const client = await pool.connect();
-    const result = await client.query('SELECT * FROM test_table');
-    const results = { 'results': (result) ? result.rows : null};
-    res.render('pages/db', results );
-    client.release();
-  } catch (err) {
-    console.error(err);
-    res.send("Error " + err);
+client.connect();
+
+client.query('SELECT table_schema,table_name FROM information_schema.tables;', (err, res) => {
+  if (err) throw err;
+  for (let row of res.rows) {
+    console.log(JSON.stringify(row));
   }
-})
+  client.end();
+});
+
 
 const PORT = process.env.PORT || 8080;
 
