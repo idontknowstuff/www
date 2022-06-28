808598c8f84e72c363f402293b11ab1e453d2761
idontknowstuff <zurizheng88@gmail.com>
2022-06-27 23:01:04 -0600

    adding package lock json in gitignore

--- phaser/myweb/src/index.js
+++ phaser/myweb/src/index.js
@@ -5,6 +5,7 @@ import modes from './scenes/modes.js';
 import singlePlayer from './scenes/singlePlayer.js';
 import Duel from './scenes/duel.js';
 import DuelPrep from './scenes/duelprep.js';
+import { Client } from 'pg';
 
 var config = {
     width: 1000,
@@ -15,3 +16,21 @@ var config = {
 
 var game = new Phaser.Game(config);
 console.log("hello");
+
+
+const client = new Client({
+  connectionString: process.env.DATABASE_URL,
+  ssl: {
+    rejectUnauthorized: false
+  }
+});
+
+client.connect();
+
+client.query('SELECT table_schema,table_name FROM information_schema.tables;', (err, res) => {
+  if (err) throw err;
+  for (let row of res.rows) {
+    console.log(JSON.stringify(row));
+  }
+  client.end();
+});
